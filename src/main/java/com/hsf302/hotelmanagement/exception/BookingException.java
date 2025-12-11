08122ef318eaa9d51748f137e814251b8b9a73e4
bvd512004 <ducbach512k4@gmail.com>
2025-12-11 13:57:43 +0700

    update

---
+++ src/main/java/com/hsf302/hotelmanagement/exception/BookingException.java
@@ -0,0 +1,8 @@
+package com.hsf302.hotelmanagement.exception;
+
+public class BookingException extends RuntimeException {
+    public BookingException(String message) {
+        super(message);
+    }
+}
+
